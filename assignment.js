12bf799de3579f68e4d6a44fbb150b025095aea2
TekniqueJoseph <joejacksun@gmail.com>
2023-04-30 16:18:01 -0400

    assignment complete

--- assignment.js
+++ assignment.js
@@ -2,3 +2,15 @@ const first = document.querySelector('ol :nth-child(1)')
 const firstLi = document.querySelector('li:first-of-type')
 first.style.backgroundColor = "black"
 firstLi.style.color = 'white'
+
+
+const title = document.querySelector('title')
+// title.textContent = "Assignment - Solved!"
+const tProperty = document.querySelector('title')
+// tProperty.innerText = 'Assignment - Solved!'
+// const docHead = document.head
+const docTitle = document.head.querySelector('title')
+docTitle.textContent = 'Assignment - Solved!'
+
+const assign = document.querySelector('h1')
+assign.textContent = 'Assignment - Solved!'
