12e793c2e92323b566459c425ccce305b18ce7e8
TekniqueJoseph <joejacksun@gmail.com>
2023-04-30 15:39:10 -0400

    DOM assignment 1

---
+++ assignment.js
@@ -0,0 +1,4 @@
+const first = document.querySelector('ol :nth-child(1)')
+const firstLi = document.querySelector('li:first-of-type')
+first.style.backgroundColor = "black"
+firstLi.style.color = 'white'
