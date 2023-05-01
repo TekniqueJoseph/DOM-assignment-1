244e8d833ff85acc2a13391adc24eeca4ffad404
TekniqueJoseph <joejacksun@gmail.com>
2023-05-01 08:44:59 -0400

    added more DOM traversal techniques

--- assignment.js
+++ assignment.js
@@ -1,7 +1,11 @@
+const ol = document.querySelector('ol')
 const first = document.querySelector('ol :nth-child(1)')
 const firstLi = document.querySelector('li:first-of-type')
+const oneLi = ol.firstElementChild
+
 first.style.backgroundColor = "black"
 firstLi.style.color = 'white'
+oneLi.style.border = '5px solid red'
 
 
 const title = document.querySelector('title')
@@ -11,6 +15,8 @@ const tProperty = document.querySelector('title')
 // const docHead = document.head
 const docTitle = document.head.querySelector('title')
 docTitle.textContent = 'Assignment - Solved!'
+ol.children[1].style.border = '5px solid gold'
 
 const assign = document.querySelector('h1')
 assign.textContent = 'Assignment - Solved!'
+ol.lastElementChild.style.border = '5px solid green'
